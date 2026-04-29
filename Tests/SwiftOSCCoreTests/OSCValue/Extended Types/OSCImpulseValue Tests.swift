28b6a7398efa48c40c7e007cf67a267e9bc8fbb5
Steffan Andrews <stef@orchetect.com>
2026-04-29 16:02:50 -0700

    Added unit tests extracted from OSCKit

---
+++ Tests/SwiftOSCCoreTests/OSCValue/Extended Types/OSCImpulseValue Tests.swift
@@ -0,0 +1,18 @@
+//
+//  OSCImpulseValue Tests.swift
+//  OSCKit • https://github.com/orchetect/OSCKit
+//  © 2020-2026 Steffan Andrews • Licensed under MIT License
+//
+
+import OSCKitCore
+import Testing
+
+@Suite struct OSCImpulseValue_Tests {
+    // MARK: - `any OSCValue` Constructors
+    
+    @Test
+    func oscValue_impulse() {
+        let val: any OSCValue = .impulse
+        #expect(val as? OSCImpulseValue == OSCImpulseValue())
+    }
+}
---
+++ Tests/SwiftOSCCoreTests/OSCValue/Extended Types/OSCNullValue Tests.swift
@@ -0,0 +1,18 @@
+//
+//  OSCNullValue Tests.swift
+//  OSCKit • https://github.com/orchetect/OSCKit
+//  © 2020-2026 Steffan Andrews • Licensed under MIT License
+//
+
+import OSCKitCore
+import Testing
+
+@Suite struct OSCNullValue_Tests {
+    // MARK: - `any OSCValue` Constructors
+    
+    @Test
+    func oscValue_null() {
+        let val: any OSCValue = .null
+        #expect(val as? OSCNullValue == OSCNullValue())
+    }
+}
