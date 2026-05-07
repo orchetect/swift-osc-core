383e7857df77e15cebc51e3772fb57f5781a252e
Steffan Andrews <stef@orchetect.com>
2026-05-06 23:01:10 -0700

    Added new `OSCUDPClientError` enum

---
+++ Sources/SwiftOSCCore/IO Common/UDP/OSCUDPClientError.swift
@@ -0,0 +1,28 @@
+//
+//  OSCUDPClientError.swift
+//  SwiftOSC Core • https://github.com/orchetect/swift-osc-core
+//  © 2026 Steffan Andrews • Licensed under MIT License
+//
+
+import protocol Foundation.LocalizedError
+
+/// Error cases thrown during the operation of an OSC UDP client.
+public enum OSCUDPClientError: LocalizedError {
+    case notStarted
+    case noRemoteHost
+    
+    public var errorDescription: String? {
+        switch self {
+        case .notStarted:
+            "The OSC UDP client has not been started yet."
+        case .noRemoteHost:
+            "A remote host was not specified."
+        }
+    }
+}
+
+extension OSCUDPClientError: Equatable { }
+
+extension OSCUDPClientError: Hashable { }
+
+extension OSCUDPClientError: Sendable { }
