d2f1aec69a09c25ed38b469fa4dee9cce3c82d71
Steffan Andrews <stef@orchetect.com>
2026-05-06 22:51:47 -0700

    `OSCTCPServerError`: Added parameter label to `clientNotFound` case

--- Sources/SwiftOSCCore/IO Common/TCP/Server/OSCTCPServerError.swift
+++ Sources/SwiftOSCCore/IO Common/TCP/Server/OSCTCPServerError.swift
@@ -9,14 +9,14 @@ import protocol Foundation.LocalizedError
 /// Error cases thrown during the operation of OSC TCP sockets.
 public enum OSCTCPServerError: LocalizedError {
     case notStarted
-    case clientNotFound(OSCTCPClientSessionID)
+    case clientNotFound(clientID: OSCTCPClientSessionID)
     
     public var errorDescription: String? {
         switch self {
         case .notStarted:
             "The OSC TCP socket has not been started yet."
-        case let .clientNotFound(id):
-            "OSC TCP client socket with ID \(id) not found (not connected)."
+        case let .clientNotFound(clientID: clientID):
+            "OSC TCP client socket with ID \(clientID) not found (not connected)."
         }
     }
 }
