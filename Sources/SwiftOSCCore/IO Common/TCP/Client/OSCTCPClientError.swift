2b54e9540b1fb0e61e867f87a3f15a9d97a5929d
Steffan Andrews <stef@orchetect.com>
2026-05-06 22:47:08 -0700

    Added new `OSCTCPClientError` enum

---
+++ Sources/SwiftOSCCore/IO Common/TCP/Client/OSCTCPClientError.swift
@@ -0,0 +1,28 @@
+//
+//  OSCTCPClientError.swift
+//  SwiftOSC Core • https://github.com/orchetect/swift-osc-core
+//  © 2026 Steffan Andrews • Licensed under MIT License
+//
+
+import protocol Foundation.LocalizedError
+
+/// Error cases thrown during the operation of an OSC TCP client.
+public enum OSCTCPClientError: LocalizedError {
+    case notStarted
+    case noRemoteHost
+    
+    public var errorDescription: String? {
+        switch self {
+        case .notStarted:
+            "The OSC TCP socket has not been started yet."
+        case .noRemoteHost:
+            "A remote host was specified at initialization or in call to send()."
+        }
+    }
+}
+
+extension OSCTCPClientError: Equatable { }
+
+extension OSCTCPClientError: Hashable { }
+
+extension OSCTCPClientError: Sendable { }
