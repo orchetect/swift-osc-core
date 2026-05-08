148cf43fea67fe84f11ea63bfb3d35a40962d0a0
Steffan Andrews <stef@orchetect.com>
2026-05-08 12:18:47 -0700

    Added `OSCTCPServerProtocol`

---
+++ Sources/SwiftOSCIOCore/TCP/Server/OSCTCPServerProtocol.swift
@@ -0,0 +1,177 @@
+//
+//  OSCTCPServerProtocol.swift
+//  SwiftOSC Core • https://github.com/orchetect/swift-osc-core
+//  © 2026 Steffan Andrews • Licensed under MIT License
+//
+
+import class Foundation.DispatchQueue
+import SwiftOSCCore
+
+/// Listens on a local port for TCP connections in order to send and receive OSC packets over the network.
+///
+/// Use this class when you are taking the role of the host and one or more remote clients will want to connect via
+/// bidirectional TCP connection.
+///
+/// A TCP connection is also generally more reliable than using the UDP protocol.
+///
+/// Since TCP is inherently a bidirectional network connection, both `OSCTCPClient` and `OSCTCPServer` can send and
+/// receive once a connection is made. Messages sent by the server are only received by the client, and vice-versa.
+///
+/// What differentiates this server class from the client class is that the server is designed to listen for inbound
+/// connections. (Whereas, the client class is designed to connect to a remote TCP server.)
+public protocol OSCTCPServerProtocol: Sendable {
+    /// Notification type.
+    typealias Notification = OSCTCPServerNotification
+    
+    /// Notification handler closure.
+    typealias NotificationHandlerBlock = @Sendable (_ notification: Notification) -> Void
+    
+    /// Initialize with a remote hostname and UDP port.
+    ///
+    /// > Note:
+    /// >
+    /// > Call ``start()`` to begin listening for connections.
+    /// > The connections may be closed at any time by calling ``stop()`` and then restarted again as needed.
+    ///
+    /// - Parameters:
+    ///   - port: Local network port to listen for inbound connections.
+    ///     If `nil` or `0`, a random available port in the system will be chosen.
+    ///   - interface: Optionally specify a network interface for which to constrain connections.
+    ///   - timeTagMode: OSC TimeTag mode. Default is recommended.
+    ///   - framingMode: TCP framing mode. Both server and client must use the same framing mode. (Default is recommended.)
+    ///   - queue: Optionally supply a custom dispatch queue for receiving OSC packets and dispatching the
+    ///     handler callback closure. If `nil`, a dedicated internal background queue will be used.
+    ///   - receiveHandler: Handler to call when OSC bundles or messages are received.
+    init(
+        port: UInt16?,
+        interface: String?,
+        timeTagMode: OSCTimeTagMode,
+        framingMode: OSCTCPFramingMode,
+        queue: DispatchQueue?,
+        receiveHandler: OSCHandlerBlock?
+    )
+
+    // MARK: - Lifecycle
+
+    /// Starts listening for inbound connections.
+    func start() throws
+    
+    /// Closes any open client connections and stops listening for inbound connection requests.
+    func stop()
+    
+    // MARK: - Communication
+    
+    /// Send an OSC bundle or message to all connected clients.
+    func send(_ oscPacket: OSCPacket) throws
+    
+    /// Send an OSC bundle to all connected clients.
+    func send(_ oscBundle: OSCBundle) throws
+    
+    /// Send an OSC message to all connected clients.
+    func send(_ oscMessage: OSCMessage) throws
+    
+    /// Send an OSC bundle or message to one or more connected clients.
+    func send(_ oscPacket: OSCPacket, toClientIDs clientIDs: [OSCTCPClientSessionID]) throws
+    
+    /// Send an OSC bundle to one or more connected clients.
+    func send(_ oscBundle: OSCBundle, toClientIDs clientIDs: [OSCTCPClientSessionID]) throws
+    
+    /// Send an OSC message to one or more connected clients.
+    func send(_ oscMessage: OSCMessage, toClientIDs clientIDs: [OSCTCPClientSessionID]) throws
+
+    // MARK: - Properties
+
+    /// Time tag mode. Determines how OSC bundle time tags are handled.
+    var timeTagMode: OSCTimeTagMode { get set }
+    
+    /// Local network port.
+    var localPort: UInt16 { get }
+    
+    /// Network interface to restrict connections to.
+    var interface: String? { get }
+    
+    /// Returns a boolean indicating whether the OSC server has been started.
+    var isStarted: Bool { get }
+    
+    /// TCP packet framing mode.
+    var framingMode: OSCTCPFramingMode { get }
+    
+    /// Set the receive handler closure.
+    /// This closure will be called when OSC bundles or messages are received.
+    func setReceiveHandler(_ handler: OSCHandlerBlock?)
+    
+    /// Set the notification handler closure.
+    /// This closure will be called when a notification is generated, such as connection and disconnection events.
+    func setNotificationHandler(_ handler: NotificationHandlerBlock?)
+    
+    /// Returns a dictionary of currently connected clients keyed by client session ID.
+    ///
+    /// > Note:
+    /// >
+    /// > A client ID is transient and only valid for the lifecycle of the connection. Client IDs are randomly-assigned
+    /// > upon each newly-made connection. For this reason, these IDs should not be stored persistently, but instead
+    /// > queried from the OSC TCP server when a client connects or analyzing currently-connected clients.
+    var clients: [OSCTCPClientSessionID: (host: String, port: UInt16)] { get }
+    
+    /// Disconnect a connected client from the server.
+    func disconnectClient(clientID: OSCTCPClientSessionID)
+}
+
+// MARK: - Defaulted parameters
+
+extension OSCTCPServerProtocol {
+    /// Initialize with a remote hostname and UDP port.
+    ///
+    /// > Note:
+    /// >
+    /// > Call ``start()`` to begin listening for connections.
+    /// > The connections may be closed at any time by calling ``stop()`` and then restarted again as needed.
+    ///
+    /// - Parameters:
+    ///   - port: Local network port to listen for inbound connections.
+    ///     If `nil` or `0`, a random available port in the system will be chosen.
+    ///   - interface: Optionally specify a network interface for which to constrain connections.
+    ///   - timeTagMode: OSC TimeTag mode. Default is recommended.
+    ///   - framingMode: TCP framing mode. Both server and client must use the same framing mode. (Default is recommended.)
+    ///   - queue: Optionally supply a custom dispatch queue for receiving OSC packets and dispatching the
+    ///     handler callback closure. If `nil`, a dedicated internal background queue will be used.
+    ///   - receiveHandler: Handler to call when OSC bundles or messages are received.
+    @_disfavoredOverload
+    public init(
+        port: UInt16?,
+        interface: String? = nil,
+        timeTagMode: OSCTimeTagMode = .ignore,
+        framingMode: OSCTCPFramingMode = .osc1_1,
+        queue: DispatchQueue? = nil,
+        receiveHandler: OSCHandlerBlock? = nil
+    ) {
+        self.init(
+            port: port,
+            interface: interface,
+            timeTagMode: timeTagMode,
+            framingMode: framingMode,
+            queue: queue,
+            receiveHandler: receiveHandler
+        )
+    }
+}
+
+// MARK: - Default implementation
+
+extension OSCTCPServerProtocol {
+    public func send(_ oscBundle: OSCBundle) throws {
+        try send(.bundle(oscBundle))
+    }
+    
+    public func send(_ oscMessage: OSCMessage) throws {
+        try send(.message(oscMessage))
+    }
+    
+    public func send(_ oscBundle: OSCBundle, toClientIDs clientIDs: [OSCTCPClientSessionID]) throws {
+        try send(.bundle(oscBundle), toClientIDs: clientIDs)
+    }
+    
+    public func send(_ oscMessage: OSCMessage, toClientIDs clientIDs: [OSCTCPClientSessionID]) throws {
+        try send(.message(oscMessage), toClientIDs: clientIDs)
+    }
+}
