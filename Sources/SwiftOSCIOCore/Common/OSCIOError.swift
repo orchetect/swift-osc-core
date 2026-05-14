fe9e7a3b5403406bc8488019dd443681d5ae1a58
Steffan Andrews <stef@orchetect.com>
2026-05-14 14:47:48 -0700

    `OSCIOError`: Cleanup

--- Sources/SwiftOSCIOCore/Common/OSCIOError.swift
+++ Sources/SwiftOSCIOCore/Common/OSCIOError.swift
@@ -8,8 +8,14 @@ import protocol Foundation.LocalizedError
 
 /// Error cases thrown during the operation of a SwiftOSC network I/O class.
 public enum OSCIOError: LocalizedError {
+    /// Client socket with the given client ID is not found (not connected).
+    case clientNotFound(clientID: OSCTCPClientSessionID)
+
     /// Invalid interface name or address.
     case invalidInterface
+
+    /// No remote host was specified.
+    case noRemoteHost
 
     /// The socket has not been started yet.
     case notStarted
@@ -17,24 +23,18 @@ public enum OSCIOError: LocalizedError {
     /// The socket has not been connected yet.
     case notConnected
 
-    /// No remote host was specified.
-    case noRemoteHost
-
-    /// Client socket with the given client ID is not found (not connected).
-    case clientNotFound(clientID: OSCTCPClientSessionID)
-
     public var errorDescription: String? {
         switch self {
+        case let .clientNotFound(clientID: clientID):
+            "Client socket with ID \(clientID) not found (not connected)."
         case .invalidInterface:
             "Invalid interface name or address."
+        case .noRemoteHost:
+            "A remote host was specified."
         case .notStarted:
             "The network socket has not been started yet."
         case .notConnected:
             "The network socket has not been connected yet."
-        case .noRemoteHost:
-            "A remote host was specified."
-        case let .clientNotFound(clientID: clientID):
-            "Client socket with ID \(clientID) not found (not connected)."
         }
     }
 }
