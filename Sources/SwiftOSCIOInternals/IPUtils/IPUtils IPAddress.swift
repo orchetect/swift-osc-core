51e94d4ec56472ca47d86612fa0735dd012823d3
Steffan Andrews <stef@orchetect.com>
2026-05-18 16:54:52 -0700

    Internals: `IPUtils`: `IPAddress`: Added inline docs and added `string` property

--- Sources/SwiftOSCIOInternals/IPUtils/IPUtils IPAddress.swift
+++ Sources/SwiftOSCIOInternals/IPUtils/IPUtils IPAddress.swift
@@ -5,15 +5,30 @@
 //
 
 extension IPUtils {
+    /// Network IP address (IPv4 / IPv6).
     public enum IPAddress: Equatable, Hashable, Sendable {
+        /// IPv4 IP address.
         case v4(String)
-        case v6(String)
         
-        public var protocolFamily: IPProtocolFamily {
-            switch self {
-            case .v4(_): .ipv4
-            case .v6(_): .ipv6
-            }
+        /// IPv6 IP address.
+        case v6(String)
+    }
+}
+
+extension IPUtils.IPAddress {
+    /// Returns the IP protocol family for the IP address.
+    public var protocolFamily: IPUtils.IPProtocolFamily {
+        switch self {
+        case .v4(_): .ipv4
+        case .v6(_): .ipv6
+        }
+    }
+    
+    /// Returns the string representation of the IP address.
+    public var string: String {
+        switch self {
+        case let .v4(string): string
+        case let .v6(string): string
         }
     }
 }
