43d8f79835c39fdd9e9af866e2136ec4e612c0d4
Steffan Andrews <stef@orchetect.com>
2026-05-18 18:14:47 -0700

    Internals: Updated IPUtils unit tests

--- Tests/SwiftOSCIOInternalsTests/IPUtils/IPUtils Tests.swift
+++ Tests/SwiftOSCIOInternalsTests/IPUtils/IPUtils Tests.swift
@@ -6,8 +6,10 @@
 
 #if canImport(FoundationEssentials)
 import struct FoundationEssentials.UUID
+import class Foundation.DispatchQueue
 #else
 import struct Foundation.UUID
+import class Foundation.DispatchQueue
 #endif
 
 @testable import SwiftOSCIOInternals
@@ -17,69 +19,112 @@ import Testing
 /// IPv4 (`127.0.0.1`) and IPv6 (`::1`) IP addresses for the `localhost` hostname.
 @Suite
 struct IPUtilsTests {
-    @Test
-    func ipAddresses_localhost() async {
-        #expect(Set(IPUtils.ipAddresses(forHostnameOrIPAddress: "localhost", families: .all)) == [.v4("127.0.0.1"), .v6("::1")])
-        #expect(Set(IPUtils.ipAddresses(forHostnameOrIPAddress: "localhost", families: [.ipv4])) == [.v4("127.0.0.1")])
-        #expect(Set(IPUtils.ipAddresses(forHostnameOrIPAddress: "localhost", families: [.ipv6])) == [.v6("::1")])
+    @Suite
+    struct ResolveTests {
+        @Test
+        func ipAddresses_localhost() async {
+            #expect(Set(IPUtils.ipAddresses(forHostnameOrIPAddress: "localhost", families: .all)) == [.v4("127.0.0.1"), .v6("::1")])
+            #expect(Set(IPUtils.ipAddresses(forHostnameOrIPAddress: "localhost", families: [.ipv4])) == [.v4("127.0.0.1")])
+            #expect(Set(IPUtils.ipAddresses(forHostnameOrIPAddress: "localhost", families: [.ipv6])) == [.v6("::1")])
+        }
+        
+        @Test
+        func ipAddresses_127_0_0_1() {
+            #expect(Set(IPUtils.ipAddresses(forHostnameOrIPAddress: "127.0.0.1", families: .all)) == [.v4("127.0.0.1")])
+            #expect(Set(IPUtils.ipAddresses(forHostnameOrIPAddress: "127.0.0.1", families: [.ipv4])) == [.v4("127.0.0.1")])
+            #expect(Set(IPUtils.ipAddresses(forHostnameOrIPAddress: "127.0.0.1", families: [.ipv6])) == [])
+        }
+        
+        @Test
+        func ipAddresses_colon_colon_1() {
+            #expect(Set(IPUtils.ipAddresses(forHostnameOrIPAddress: "::1", families: .all)) == [.v6("::1")])
+            #expect(Set(IPUtils.ipAddresses(forHostnameOrIPAddress: "::1", families: [.ipv4])) == [])
+            #expect(Set(IPUtils.ipAddresses(forHostnameOrIPAddress: "::1", families: [.ipv6])) == [.v6("::1")])
+        }
+        
+        @Test
+        func ipAddresses_edgeCases() {
+            #expect(Set(IPUtils.ipAddresses(forHostnameOrIPAddress: "", families: .all)) == [])
+            #expect(Set(IPUtils.ipAddresses(forHostnameOrIPAddress: " ", families: .all)) == [])
+            #expect(Set(IPUtils.ipAddresses(forHostnameOrIPAddress: ".", families: .all)) == [])
+            #expect(Set(IPUtils.ipAddresses(forHostnameOrIPAddress: UUID().uuidString, families: .all)) == [])
+        }
+        
+        @Test
+        func ipAddress_localhost() {
+            #expect(IPUtils.ipAddress(forHostnameOrIPAddress: "localhost", family: .ipv4) == "127.0.0.1")
+            #expect(IPUtils.ipAddress(forHostnameOrIPAddress: "localhost", family: .ipv6) == "::1")
+            #expect(IPUtils.ipAddress(forHostnameOrIPAddress: "localhost", family: .unix) == nil)
+        }
+        
+        @Test
+        func ipAddress_127_0_0_1() {
+            #expect(IPUtils.ipAddress(forHostnameOrIPAddress: "127.0.0.1", family: .ipv4) == "127.0.0.1")
+            #expect(IPUtils.ipAddress(forHostnameOrIPAddress: "127.0.0.1", family: .ipv6) == nil)
+            #expect(IPUtils.ipAddress(forHostnameOrIPAddress: "127.0.0.1", family: .unix) == nil)
+        }
+        
+        @Test
+        func ipAddress_colon_colon_1() {
+            #expect(IPUtils.ipAddress(forHostnameOrIPAddress: "::1", family: .ipv4) == nil)
+            #expect(IPUtils.ipAddress(forHostnameOrIPAddress: "::1", family: .ipv6) == "::1")
+            #expect(IPUtils.ipAddress(forHostnameOrIPAddress: "::1", family: .unix) == nil)
+        }
+        
+        @Test
+        func hostnames() {
+            #expect(Set(IPUtils.hostnames(forHostnameOrIPAddress: "localhost")) == ["localhost"])
+            #expect(Set(IPUtils.hostnames(forHostnameOrIPAddress: "127.0.0.1")) == ["localhost"])
+            #expect(Set(IPUtils.hostnames(forHostnameOrIPAddress: "::1")) == ["localhost"])
+        }
+        
+        @Test
+        func hostnames_edgeCases() {
+            #expect(Set(IPUtils.hostnames(forHostnameOrIPAddress: "")) == [])
+            #expect(Set(IPUtils.hostnames(forHostnameOrIPAddress: " ")) == [])
+            #expect(Set(IPUtils.hostnames(forHostnameOrIPAddress: ".")) == [])
+            #expect(Set(IPUtils.hostnames(forHostnameOrIPAddress: UUID().uuidString)) == [])
+        }
     }
     
-    @Test
-    func ipAddresses_127_0_0_1() {
-        #expect(Set(IPUtils.ipAddresses(forHostnameOrIPAddress: "127.0.0.1", families: .all)) == [.v4("127.0.0.1")])
-        #expect(Set(IPUtils.ipAddresses(forHostnameOrIPAddress: "127.0.0.1", families: [.ipv4])) == [.v4("127.0.0.1")])
-        #expect(Set(IPUtils.ipAddresses(forHostnameOrIPAddress: "127.0.0.1", families: [.ipv6])) == [])
-    }
-    
-    @Test
-    func ipAddresses_colon_colon_1() {
-        #expect(Set(IPUtils.ipAddresses(forHostnameOrIPAddress: "::1", families: .all)) == [.v6("::1")])
-        #expect(Set(IPUtils.ipAddresses(forHostnameOrIPAddress: "::1", families: [.ipv4])) == [])
-        #expect(Set(IPUtils.ipAddresses(forHostnameOrIPAddress: "::1", families: [.ipv6])) == [.v6("::1")])
-    }
-    
-    @Test
-    func ipAddresses_edgeCases() {
-        #expect(Set(IPUtils.ipAddresses(forHostnameOrIPAddress: "", families: .all)) == [])
-        #expect(Set(IPUtils.ipAddresses(forHostnameOrIPAddress: " ", families: .all)) == [])
-        #expect(Set(IPUtils.ipAddresses(forHostnameOrIPAddress: ".", families: .all)) == [])
-        #expect(Set(IPUtils.ipAddresses(forHostnameOrIPAddress: UUID().uuidString, families: .all)) == [])
-    }
-    
-    @Test
-    func ipAddress_localhost() {
-        #expect(IPUtils.ipAddress(forHostnameOrIPAddress: "localhost", family: .ipv4) == "127.0.0.1")
-        #expect(IPUtils.ipAddress(forHostnameOrIPAddress: "localhost", family: .ipv6) == "::1")
-        #expect(IPUtils.ipAddress(forHostnameOrIPAddress: "localhost", family: .unix) == nil)
-    }
-    
-    @Test
-    func ipAddress_127_0_0_1() {
-        #expect(IPUtils.ipAddress(forHostnameOrIPAddress: "127.0.0.1", family: .ipv4) == "127.0.0.1")
-        #expect(IPUtils.ipAddress(forHostnameOrIPAddress: "127.0.0.1", family: .ipv6) == nil)
-        #expect(IPUtils.ipAddress(forHostnameOrIPAddress: "127.0.0.1", family: .unix) == nil)
-    }
-    
-    @Test
-    func ipAddress_colon_colon_1() {
-        #expect(IPUtils.ipAddress(forHostnameOrIPAddress: "::1", family: .ipv4) == nil)
-        #expect(IPUtils.ipAddress(forHostnameOrIPAddress: "::1", family: .ipv6) == "::1")
-        #expect(IPUtils.ipAddress(forHostnameOrIPAddress: "::1", family: .unix) == nil)
-    }
-    
-    @Test
-    func hostnames() {
-        #expect(Set(IPUtils.hostnames(forHostnameOrIPAddress: "localhost")) == ["localhost"])
-        #expect(Set(IPUtils.hostnames(forHostnameOrIPAddress: "127.0.0.1")) == ["localhost"])
-        #expect(Set(IPUtils.hostnames(forHostnameOrIPAddress: "::1")) == ["localhost"])
-    }
-    
-    @Test
-    func hostnames_edgeCases() {
-        #expect(Set(IPUtils.hostnames(forHostnameOrIPAddress: "")) == [])
-        #expect(Set(IPUtils.hostnames(forHostnameOrIPAddress: " ")) == [])
-        #expect(Set(IPUtils.hostnames(forHostnameOrIPAddress: ".")) == [])
-        #expect(Set(IPUtils.hostnames(forHostnameOrIPAddress: UUID().uuidString)) == [])
+    @Suite
+    struct ConcurrencyTests {
+        @MainActor @Test
+        func callFromMainActorCall() {
+            #expect(Set(IPUtils.ipAddresses(forHostnameOrIPAddress: "localhost", families: .all)) == [.v4("127.0.0.1"), .v6("::1")])
+            #expect(IPUtils.ipAddress(forHostnameOrIPAddress: "localhost", family: .ipv4) == "127.0.0.1")
+            #expect(Set(IPUtils.hostnames(forHostnameOrIPAddress: "localhost")) == ["localhost"])
+        }
+        
+        @Test
+        func callFromMainQueue() async {
+            await withCheckedContinuation { continuation in
+                DispatchQueue.main.async {
+                    #expect(Set(IPUtils.ipAddresses(forHostnameOrIPAddress: "localhost", families: .all)) == [.v4("127.0.0.1"), .v6("::1")])
+                    #expect(IPUtils.ipAddress(forHostnameOrIPAddress: "localhost", family: .ipv4) == "127.0.0.1")
+                    #expect(Set(IPUtils.hostnames(forHostnameOrIPAddress: "localhost")) == ["localhost"])
+                    continuation.resume()
+                }
+            }
+        }
+        
+        @Test
+        func callFromGlobalQueue() {
+            DispatchQueue.global().sync {
+                #expect(Set(IPUtils.ipAddresses(forHostnameOrIPAddress: "localhost", families: .all)) == [.v4("127.0.0.1"), .v6("::1")])
+                #expect(IPUtils.ipAddress(forHostnameOrIPAddress: "localhost", family: .ipv4) == "127.0.0.1")
+                #expect(Set(IPUtils.hostnames(forHostnameOrIPAddress: "localhost")) == ["localhost"])
+            }
+        }
+        
+        @Test
+        func callFromTask() async {
+            _ = await Task {
+                #expect(Set(IPUtils.ipAddresses(forHostnameOrIPAddress: "localhost", families: .all)) == [.v4("127.0.0.1"), .v6("::1")])
+                #expect(IPUtils.ipAddress(forHostnameOrIPAddress: "localhost", family: .ipv4) == "127.0.0.1")
+                #expect(Set(IPUtils.hostnames(forHostnameOrIPAddress: "localhost")) == ["localhost"])
+            }.value
+        }
     }
     
     #if !GITHUB_ACTIONS
